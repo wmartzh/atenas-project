dcd618aa3ae663144fe58d9ec94ac3af52c5cb42
ISC-LC4-PC10\Lab ISC 10 <jfallas334@gmail.com>
2024-02-27 16:53:37 -0600

    cuenta completa aun falta modificar los posicionamientos

--- src/routes/auth/create-account/+page.svelte
+++ src/routes/auth/create-account/+page.svelte
@@ -1,19 +1,35 @@
 <script>
  
- let name = "Masculino"
- let textos = "Femenino"
+let genero = ["Masculino","Femenino"]
+let radio = ""
+
 
 
 </script>
 
 
-<div class="avatar online hero-content text-center">
+<div class="">
    
   <div class="w-24 rounded-full">
-    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+    <h1  class = "font-family: Segoe UI"> ATENAS </h1>
   </div>
-  <h1 class="hero-content text-center" >CREATE ACCOUNT</h1>
+  <span class="hero-content text-center box-decoration-clone bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2 ..." >
+    <label class="btn btn-circle swap swap-rotate">
+  
+   
+      <input type="checkbox" />
+      
+      
+      <svg class="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="58" height="32" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z"/></svg>
+      
+      
+      <svg class="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"/></svg>
+      
+    </label>
+    <br>CREATE ACCOUNT</span>
+    
 </div>
+
 
 
 
@@ -21,7 +37,7 @@
   <div class="hero-content text-center">
     <div class="max-w-md">
       
-     <h1 class="hero-content text-center" >CREATE ACCOUNT</h1>
+ 
 
       <div>
         <h2>Carnet</h2>
@@ -59,8 +75,11 @@
                
                <div>
                 <h2>Genero</h2>
-               <input type="radio" name="radio-1" class="radio" checked  />
-                   <input type="radio" name="radio-1" class="radio" />Femenino
+                {#each genero  as generos }
+                <input type="radio" value="{generos}" bind:group={radio} > {genero}
+                {/each }
+               
+                  
                   </div>
 
 
