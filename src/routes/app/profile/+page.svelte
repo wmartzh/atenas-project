beb4b0b90db23f8333331b95db02e03ecfbbf33d
DESKTOP-7EB4SI0\Dell <hanyvalverdec@gmail.com>
2024-02-27 13:26:24 -0600

    Import forms

--- src/routes/app/profile/+page.svelte
+++ src/routes/app/profile/+page.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-    
-    export let data: PageData;
+	import Profile from '$lib/components/profile.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
 </script>
 
 <h1>Hello this is profile</h1>
+
+<div>
+	<Profile />
+</div>
+
+
