974779e899018b0f03216f73900a512d34209843
Malcon-25 <158131554+Malcon-25@users.noreply.github.com>
2024-02-27 10:23:19 -0600

    Add Table (#2)
    * Crear Componente para generation de tablas
    
    * Crear Componente para generation de tablas

---
+++ src/lib/components/table.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    export let data: any[];
+    export let titles: { label: string; key: string }[];
+  </script>
+  <div class="overflow-x-auto">
+  <table class="table table-xs">
+    <thead>
+      <tr>
+        {#each titles as title (title.key)}
+          <th>{title.label}</th>
+        {/each}
+      </tr>
+    </thead>
+    <tbody>
+      {#each data as row}
+        <tr>
+          {#each titles as title (title.key)}
+            <td>{row[title.key]}</td>
+          {/each}
+        </tr>
+      {/each}
+    </tbody>
+</table>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="hero min-h-screen bg-base-200">
   <div class="hero-content text-center">
     <div class="max-w-md">
-      <h1 class="text-5xl font-bold">Hello there</h1>
+      <h1 class="text-5xl font-bold">Hello there </h1>
     </div>
   </div>
 </div>
