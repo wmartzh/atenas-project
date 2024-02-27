7087d7db9680ecf133366c4a5f2699c3fa6ea100
ISC-LC4-PC10\Lab ISC 10 <jfallas334@gmail.com>
2024-02-26 21:37:25 -0600

    crear cuenta

--- src/routes/auth/create-account/+page.svelte
+++ src/routes/auth/create-account/+page.svelte
@@ -1,5 +1,72 @@
-<script lang="ts">
+<script>
+ 
+ let name = "Masculino"
+ let textos = "Femenino"
+
 
 </script>
 
-<h1>Create new Account</h1>
+
+<div class="avatar online hero-content text-center">
+   
+  <div class="w-24 rounded-full">
+    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+  </div>
+  <h1 class="hero-content text-center" >CREATE ACCOUNT</h1>
+</div>
+
+
+
+<div class="hero min-h-screen bg-base-200">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      
+     <h1 class="hero-content text-center" >CREATE ACCOUNT</h1>
+
+      <div>
+        <h2>Carnet</h2>
+      <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+         </div> 
+     
+      <div>
+        <h2>Nombres</h2>
+      <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+         </div> 
+
+         <div>
+          <h2>Apellidos</h2>
+        <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+           </div> 
+
+           <div>
+            <h2>Rol</h2>
+            <select class="select select-bordered w-full max-w-xs">
+              <option disabled selected></option>
+              <option>Administrador</option>
+              <option>Estudiante</option>
+            </select>
+             </div> 
+      
+           <div>
+            <h2>Email</h2>
+          <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+             </div>   
+
+             <div>
+              <h2>Contraseña</h2>
+            <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+               </div> 
+               
+               <div>
+                <h2>Genero</h2>
+               <input type="radio" name="radio-1" class="radio" checked  />
+                   <input type="radio" name="radio-1" class="radio" />Femenino
+                  </div>
+
+
+               <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Confirmar</button>
+
+    </div>
+  </div>
+</div>
+
