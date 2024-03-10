8375b3f0672a833d494524bbcd9d6acc3ddd3897
AidenAlfaro <109189894+AidenAlfaro@users.noreply.github.com>
2024-03-10 11:46:06 -0600

    AddingEventTypes (#14)
    * AddingEventTypes
    
    * Changed status to enum

---
+++ src/lib/types/event.ts
@@ -0,0 +1,32 @@
+import { updated } from "$app/stores";
+import { s } from "vitest/dist/reporters-1evA5lom.js";
+  
+  // Create the event type with status as an enum
+  export type Event = {
+    id: string;
+    title: string;
+    start: string;
+    end: string;
+    status: EventStatus;
+    weekDay: weekDays;
+    scheduleDate: string;
+    createdAt: string;
+    updatedAt: string;
+  };
+  
+  // Define the weekDays enum
+  enum weekDays {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+  }
+  
+enum EventStatus {
+    Pending,
+    Confirmed,
+    Canceled
+  }
--- src/server/services/index.ts
+++ src/server/services/index.ts
@@ -1,3 +1,4 @@
 export * from './user.service'
 export * from './profile.service'
 export * from './auth.service'
+export * from './event.service'
