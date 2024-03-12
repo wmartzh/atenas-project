74ebeaf749ec8b5af10d092acb72691f91f31147
Aiden Alfaro <aalfaro1503@gmail.com>
2024-03-12 13:36:46 -0600

    Commiting the event types file

--- src/lib/types/event.ts
+++ src/lib/types/event.ts
@@ -1,32 +1,33 @@
+// events.ts
+
 import { updated } from "$app/stores";
-import { s } from "vitest/dist/reporters-1evA5lom.js";
-  
-  // Create the event type with status as an enum
-  export type Event = {
-    id: string;
-    title: string;
-    start: string;
-    end: string;
-    status: EventStatus;
-    weekDay: weekDays;
-    scheduleDate: string;
-    createdAt: string;
-    updatedAt: string;
-  };
-  
-  // Define the weekDays enum
-  enum weekDays {
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-    Saturday,
-    Sunday
-  }
-  
+import type { Event, weekDays, EventStaztus } from "../../lib/types/event"; 
+
+enum weekDays {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday
+}
+
 enum EventStatus {
-    Pending,
-    Confirmed,
-    Canceled
-  }
+  Pending,
+  Confirmed,
+  Canceled
+}
+
+export type Event = {
+  id: string;
+  title: string;
+  start: string;
+  end: string;
+  status: EventStatus;
+  weekDay: weekDays;
+  scheduleDate: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
