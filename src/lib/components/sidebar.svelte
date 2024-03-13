df17c6c2c1cd595f7699ede0308202ab03c6da9e
EMiralda <josemiralda16@gmail.com>
2024-03-12 21:02:33 -0600

    CU-86azkpc3h - Diseño > Arreglar Sidebar

--- src/lib/components/sidebar.svelte
+++ src/lib/components/sidebar.svelte
@@ -1,8 +1,10 @@
-<div class="drawer-side lg:shadow-md ">
-	<label for="sidebar" aria-label="close sidebar" class="drawer-overlay"></label>
-	<ul class="menu bg-base p-4 flex flex-col shadow-md relative h-screen gap-y-1 w-[200px]">
-		<slot name="header" />
-		<slot />
-		<slot name="footer" />
-	</ul>
+<script>
+</script>
+<div class="drawer-side lg:shadow-md dark:bg-gray-900">
+    <label for="sidebar" aria-label="close sidebar" class="drawer-overlay"></label>
+    <ul class="menu dark:bg-gray-900 bg-base-100 p-4 flex flex-col shadow-md relative h-screen gap-y-1 w-[200px]">
+        <slot name="header" />
+        <slot />
+        <slot name="footer" />
+    </ul>
 </div>
